956b896ec512d0990885b881c5a45869e356b3fe
Ahsan Awadullah Azizan <ahsan_azizan_31rpl@student.smktelkom-mlg.sch.id>
2022-11-23 06:29:03 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,23 +3,25 @@ from tweeter_api import send_tweet
 import datetime as dt
 import time
 
-start_time = time.time()
-
 
 def tweet():
     message = f"This tweet is sent at :\nDate   : {dt.datetime.now().date()}\nTime   : {dt.datetime.now().time()} (WIB)" \
               f"\n\nThis tweet is sent by a bot"
-
     send_tweet(message, "image.jpg")
 
 
-lim = int(input("Tweet Limit : "))
-counter = 0
-delay = float(input("Delay : "))
+def activate_bot():
+    start_time = time.time()
+    lim = int(input("Tweet Limit : "))
+    counter = 0
+    delay = float(input("Delay : "))
 
-while counter < lim:
-    tweet()
-    counter += 1
-    time.sleep(delay - ((time.time() - start_time) % delay))
+    while counter < lim:
+        tweet()
+        counter += 1
+        time.sleep(delay - ((time.time() - start_time) % delay))
 
 
+if __name__ == "__main__":
+    activate_bot()
+    
